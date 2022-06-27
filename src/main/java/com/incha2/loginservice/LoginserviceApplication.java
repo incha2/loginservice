315c8ab8a2c6996954486871da71410139e52f90
Indrajit Chakraborty <indrachakraborty20@gmail.com>
2022-06-27 20:18:54 +0530

    Added web security
    Added web security
    
    Co-Authored-By: Indrajit Chakraborty <indrachakraborty20@gmail.com>

--- src/main/java/com/incha2/loginservice/LoginserviceApplication.java
+++ src/main/java/com/incha2/loginservice/LoginserviceApplication.java
@@ -2,8 +2,10 @@ package com.incha2.loginservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.incha2.loginservice.*")
 public class LoginserviceApplication {
 
 	public static void main(String[] args) {
